3d3e24407944c77b2312d76dcc8b09562bcd46b0
Nannan <1041836312@qq.com>
2021-04-03 15:46:10 +0800

    更新依赖.net4.7.2

--- DofLauncher/Properties/AssemblyInfo.cs
+++ DofLauncher/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("DofLauncher")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("滑里稽登陆器")]
+[assembly: AssemblyDescription("dof登陆器")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DofLauncher")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCompany("nnn")]
+[assembly: AssemblyProduct("https://github.com/nnn149/DofLauncher")]
+[assembly: AssemblyCopyright("Copyright ©  2021 Nannan")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -51,5 +51,6 @@ using System.Windows;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0")]
+[assembly: AssemblyFileVersion("1.0")]
+[assembly: NeutralResourcesLanguage("zh-Hans")]
