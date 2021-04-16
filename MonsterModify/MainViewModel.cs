e5721d159bd48c935b6dff6207a98fbdd3961630
Nannan <1041836312@qq.com>
2021-04-17 00:48:19 +0800

    mvvm
    实现
    Command
    INotifyPropertyChanged

---
+++ MonsterModify/MainViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Toolkit.Mvvm.ComponentModel;
+using MonsterModify.Annotations;
+
+namespace MonsterModify
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private int _allMonsterAttributeListIndex;
+
+        public int AllMonsterAttributeListIndex
+        {
+            get => _allMonsterAttributeListIndex;
+            set
+            {
+                _allMonsterAttributeListIndex = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand TestCommand { get; set; }
+
+        public MainViewModel()
+        {
+            TestCommand = new RelayCommand(Test);
+        }
+
+        private void Test()
+        {
+            AllMonsterAttributeListIndex++;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ MonsterModify/RelayCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace MonsterModify
+{
+    public class RelayCommand : ICommand
+    {
+        public Action execute;
+
+        public RelayCommand(Action action)
+        {
+            execute = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (execute != null)
+            {
+                execute();
+            }
+        }
+
+        public event EventHandler? CanExecuteChanged;
+    }
+}
